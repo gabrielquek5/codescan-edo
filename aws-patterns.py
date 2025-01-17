cb4de7d697a2152541ed970b69a775c0c0a30053
gabrielquek5 <119473080+gabrielquek5@users.noreply.github.com>
2025-01-17 17:25:33 +0800

    Update aws-patterns.py 9

--- aws-patterns.py
+++ aws-patterns.py
@@ -7,7 +7,7 @@ def get_secret():
     # Hard-coded secrets (CWE-798)
     return {
         'username': 'admin',
-        'password': 'super_secret_password321abcqwerty!'
+        'password': 'super_secret_password321abcqwertyjkl!'
     }
 
 def process_user_input(user_data):
